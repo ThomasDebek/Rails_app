452856a065581b5f445e083b8bf050c22ba08d4a
ThomasDebek <tom.debek@gmail.com>
2014-06-05 12:31:19 +0200

    Pierwsze testy :). Nasze testy oczekuja od stron ze beda zawieraly ich nazwy, wiec im je dajmy modyfikujac widoki

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,40 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+
+  describe "Home page" do
+    it "should have the content 'Find a place on earth. ' "   do
+      visit '/static_pages/home'
+      expect(page).to have_content('Find a place on earth.')
     end
   end
+
+  describe "Help page" do
+    it "should have the content 'Help' " do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About' " do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact' " do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+  end
+
+  describe "Browse page" do
+    it "should have the content 'Browse' " do
+      visit '/static_pages/browse'
+      expect(page).to have_content('Browse')
+    end
+  end
+
 end
