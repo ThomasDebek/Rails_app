8fd42a29834921fdc915e6f950253751427f3a82
ThomasDebek <tom.debek@gmail.com>
2014-06-06 14:31:57 +0200

    Uproscmy nasze testy

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,67 +5,61 @@ describe "StaticPages" do
 
 
   describe "Home page" do
+    before { visit root_path }
     it "should have the content 'Find a place on earth. ' "   do
-      visit root_path
       expect(page).to have_content('Find a place on earth.')
     end
 
     it "should habe the base title " do
-      visit root_path
       expect(page).to have_content('Rails app')
     end
 
 
     it "should have the title 'Home' " do
-      visit root_path
       expect(page).to have_title(" | Home")
     end
   end
 
 
   describe "Help page" do
+    before { visit help_path }
     it "should have the content 'Help' " do
-      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the title 'Help' " do
-      visit help_path
       expect(page).to have_title("  | Help")
     end
   end
 
 
   describe "About page" do
+    before { visit about_path }
     it "should have the content 'About' " do
-      visit about_path
       expect(page).to have_content('About')
     end
     it "should have the title 'About' " do
-      visit about_path
       expect(page).to have_title("  | About")
     end
   end
 
 
   describe "Contact page" do
+    before { visit contact_path }
     it "should have the content 'Contact' " do
-      visit contact_path
       expect(page).to have_content('Contact')
     end
     it "should have the title 'Contact' " do
-      visit contact_path
       expect(page).to have_title("  | Contact")
     end
   end
 
 
   describe "Browse page" do
+    before { visit browse_path }
     it "should have the content 'Browse' " do
-      visit browse_path
       expect(page).to have_content('Browse')
     end
     it "should have the title 'Browse '" do
-      visit browse_path
       expect(page).to have_title("  | Browse")
     end
   end
