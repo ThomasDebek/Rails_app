d3dd6aa5c3c7d630c4a283449ae39a9908aacb42
ThomasDebek <tom.debek@gmail.com>
2014-06-06 15:01:33 +0200

    Uproscmy nasze testy jeszcze bardziej, dodajmy subjecta. Teraz nasze testy sa bardziej czytelne :)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,66 +2,47 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-
+  subject { page }
 
   describe "Home page" do
     before { visit root_path }
-    it "should have the content 'Find a place on earth. ' "   do
-      expect(page).to have_content('Find a place on earth.')
-    end
 
-    it "should habe the base title " do
-      expect(page).to have_content('Rails app')
-    end
-
-
-    it "should have the title 'Home' " do
-      expect(page).to have_title(" | Home")
-    end
+    it { should have_content('Find a place on earth.') }
+    it { should have_title("RailsApp") }
+    it { should have_title('| Home') }
   end
 
 
   describe "Help page" do
     before { visit help_path }
-    it "should have the content 'Help' " do
-      expect(page).to have_content('Help')
-    end
-    it "should have the title 'Help' " do
-      expect(page).to have_title("  | Help")
-    end
+
+    it { should have_content('Help') }
+    it { should have_title('Help') }
   end
+
 
 
   describe "About page" do
     before { visit about_path }
-    it "should have the content 'About' " do
-      expect(page).to have_content('About')
-    end
-    it "should have the title 'About' " do
-      expect(page).to have_title("  | About")
-    end
+
+    it { should have_content('About') }
+    it { should have_title('About') }
   end
 
 
   describe "Contact page" do
     before { visit contact_path }
-    it "should have the content 'Contact' " do
-      expect(page).to have_content('Contact')
-    end
-    it "should have the title 'Contact' " do
-      expect(page).to have_title("  | Contact")
-    end
+
+    it { should have_content('Contact') }
+    it { should have_title('Contact') }
   end
 
 
   describe "Browse page" do
     before { visit browse_path }
-    it "should have the content 'Browse' " do
-      expect(page).to have_content('Browse')
-    end
-    it "should have the title 'Browse '" do
-      expect(page).to have_title("  | Browse")
-    end
+
+    it { should have_content('Browse') }
+    it { should have_title('Browse') }
   end
 
 end
