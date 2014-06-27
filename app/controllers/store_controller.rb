dac357ad4eca8452cd3dcae882aedd6b6b4ca9e8
ThomasDebek <tom.debek@gmail.com>
2014-06-27 15:08:22 +0200

    Usawmy w kontrolerze ilosc wyswietlanych produktow, oraz zmodyfikujmy widok

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,6 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @places = Place.order(:title)
+    @places = Place.all.paginate(page: params[:page], per_page: 6)
   end
 end
