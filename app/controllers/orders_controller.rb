b3dd1d503708870fc1b212460b4b7fd9bb187c4d
ThomasDebek <tom.debek@gmail.com>
2014-06-27 13:46:59 +0200

    Ograniczmy dostep, aby ludzie bez logowania nie mieli dostepu do modyfikacji naszych zasobow. Dodamy metode authorize, ktora skutecznie zablokuje dostep. W miejscu gdzie chcemy ja wprowadzic dodajmy kolbeacka - before_action: authorize

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authorize
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
