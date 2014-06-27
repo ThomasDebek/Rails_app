3d9f866733b9543e7e94a14700ad2cc5e75c1ba6
ThomasDebek <tom.debek@gmail.com>
2014-06-27 15:12:04 +0200

    Odrobina paginacji w innym miejscu

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,7 @@ class PlacesController < ApplicationController
   # GET /places
   # GET /places.json
   def index
-    @places = Place.all
+    @places = Place.all.paginate(page: params[:page], per_page: 6)
   end
 
   # GET /places/1
