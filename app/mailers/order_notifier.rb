d0a66a7c174f51bc2f7ed9f38f44d403745d81a7
ThomasDebek <tom.debek@gmail.com>
2014-09-27 13:33:23 +0200

    Zrefaktoryzujmy nasze metody mailera w order_notifier.rb

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -6,10 +6,10 @@ class OrderNotifier < ActionMailer::Base
   #
   #   en.order_notifier.received.subject
   #
-  def received
-    @greeting = "Hi"
+  def received(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -18,8 +18,8 @@ class OrderNotifier < ActionMailer::Base
   #   en.order_notifier.shipped.subject
   #
   def shipped
-    @greeting = "Hi"
+    @greeting = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 end
